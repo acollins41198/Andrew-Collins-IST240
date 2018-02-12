96307ec514c7229d431f3f7e5d5aeb465eca1038
acollins41198 <acollins41198@gmail.com>
2018-02-12 14:16:56 -0500

    Submitting Lab42

--- MyWebApp/MyWebApp/Controllers/HomeController.cs
+++ MyWebApp/MyWebApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MyWebApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Collins Calculators";
 
             return View();
         }
@@ -31,5 +31,36 @@ namespace MyWebApp.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult CalcApp(string ValA, string ValB, string Op) {
+            Console.WriteLine("test Write");
+            double value1 = Convert.ToDouble(ValA);
+            double value2 = Convert.ToDouble(ValB);
+            int selectionType = Convert.ToInt32(Op);
+            double result = 0;
+            switch (selectionType)
+            {
+                case 1:
+                    result = value1 + value2;
+                    break;
+                case 2:
+                    result = value1 - value2;
+                    break;
+                case 3:
+                    result = value1 * value2;
+                    break;
+                case 4:
+                    result = value1 / value2;
+                    break;
+                default:
+                    Console.WriteLine("Error parsing math");
+                    break;
+            }
+            
+ 
+            return View("CalcApp", (Object)Convert.ToString(result));
+        }
+
     }
 }
